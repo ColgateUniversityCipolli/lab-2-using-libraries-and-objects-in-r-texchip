c79952f293e8627ee52a0089b5af4c8867f60aa3
texchip <100250142+texchip@users.noreply.github.com>
2025-01-30 15:04:01 -0500

    Part 2 completed.

--- pleclerc_lab2.R
+++ pleclerc_lab2.R
@@ -3,24 +3,29 @@ library(stringr)
 music.subdirs <- list.dirs("MUSIC") # vector with all subdirectories of MUSIC/
 album.indices <- which(str_count(music.subdirs, pattern = "/")==2) # isolate albums
 albums <- music.subdirs[album.indices] # vector with all albums
+batfile <- file("batfile.txt", open = "wt")
 
-#for(i in 1:length(albums)){ # albums
-#}
-album.subdirs <- list.files(albums[1]) # vector with all files in album directory
-song.indices <- which(str_count(album.subdirs, pattern = ".wav")==1) # isolate the .wav files
-songs <- album.subdirs[song.indices] # vector with all songs
+for(i in 1:length(albums)){ # albums
+  
+  album.subdirs <- list.files(albums[i]) # vector with all files in album directory
+  song.indices <- which(str_count(album.subdirs, pattern = ".wav")==1) # isolate the .wav files
+  songs <- album.subdirs[song.indices] # vector with all songs
+  code.to.process <- rep(NA, length(albums[i])) # empty vector with size equal to album size
+  
+  for(j in 1:length(songs)){ # songs
+    
+    track.file.location <- paste(albums[i],"/",songs[j], sep = "") # track file path
+    track.file <- str_sub(songs[j], end = -5) # remove .wav
+    track.file <- str_split(track.file, "-", simplify = T)
+    track.name <- track.file[1,3]
+    artist.name <- track.file[1,2]
+    album.name <- str_split(albums[i], "/", simplify = T)[1,3]
+ 
+    output.file <- paste(artist.name,"-",album.name,"-",track.name,".json", sep = "")
+    code.to.process[j] <- paste("streaming_extractor_music.exe",songs[j],output.file, sep = " ")
+  }
+  
+  writeLines(code.to.process, batfile, sep = "\n")
+}
 
-#for(j in 1:length(songs)){ # songs
-#}
-
-code.to.process <- rep(NA, length(albums[1])) # empty vector with size equal to album size
-track.file.location <- paste(albums[1],"/",songs[1], sep = "") # track file path
-track.file <- str_sub(songs[1], end = -5) # remove .wav
-track.file <- str_split(track.file, "-", simplify = T)
-track.name <- track.file[1,3]
-artist.name <- track.file[1,2]
-album.name <- str_split(albums[1], "/", simplify = T)[1,3]
-
-output.file <- paste(artist.name,"-",album.name,"-",track.name,".json", sep = "")
-
-
+close(batfile)
